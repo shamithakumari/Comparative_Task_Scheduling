e39aa6526d95cf124a222515d3eb2c4bea38f4da
Shamitha <shamithakumarims@gmail.com>
2022-06-03 21:26:15 +0530

    final touch-up

--- comparative_study_cloud/src/Comparative_study.java
+++ comparative_study_cloud/src/Comparative_study.java
@@ -2,30 +2,19 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-import org.cloudbus.cloudsim.Log;
+
 
 import FCFS.CloudSimFCFS;
 import RoundRobin.CloudSimRR;
 import SJF.CloudSimSJF;
-//
-//
-//import java.util.SortedMap;
-//import java.util.TreeMap;
-//import java.util.concurrent.TimeUnit;
+
 
 public class Comparative_study {
  
 
 	public static void main(String[] args)  {
     	double newValue;
-//        SortedMap<Double, String> map = new TreeMap<>();
-//        
-//         double FCFSS_finishtime=CloudSimFCFS.FCFS_FinishTime;
-//        System.out.println("Sorted list of algorithms (criteria: earliest finish time)");
-//        for(double i: map.keySet()){
-//            System.out.printf("%s: %.2f%n", map.get(i), i);
-//        
-//        System.out.println("===========================================");
+
     	
     	
 //    	System.out.println("hello");
@@ -37,7 +26,6 @@ public class Comparative_study {
     	CloudSimRR.main(new String[0]);
     	System.out.println("\n===========================================\n");
     	
-//     	System.out.println("finish time="+ CloudSimFCFS.FCFS_FinishTime);
      	
      	double fcfsFinishTime = CloudSimFCFS.FCFS_FinishTime;
      	double sjfFinishTime = CloudSimSJF.SJF_FinishTime;
@@ -69,10 +57,10 @@ public class Comparative_study {
         
       	System.out.println("\n===========================================\n");
       	System.out.printf("Round Robin=%.4f\n",results.get(0));
-      	System.out.printf("Shortest Job First=%.4f\n",results.get(1));
-      	System.out.printf("First Come First Serve=%.4f\n",results.get(2));
+      	System.out.printf("First Come First Serve=%.4f\n",results.get(1));
+      	System.out.printf("Shortest Job First=%.4f\n",results.get(2));
      	System.out.println("\n===========================================\n");
-        System.out.println("Algorithm that gives the best result is: Round Robin Algorithm");
+        System.out.println("Algorithm that gives the best result is(criteria: earliest finish time): Round Robin Algorithm");
         
         
     }
